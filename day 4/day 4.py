6de21f4693e58aa646d7c576ecffba98d03b1eca
chavle77867@gmail.com <chavle77867@gmail.com>
2024-02-18 23:27:48 +0400

    second commit

---
+++ day 4/day 4.py
@@ -0,0 +1,11 @@
+name=input("please enter your name")
+
+print("momxmareblis saxelia:" + name)
+
+age=input("please enter your age: ")
+
+print("momxmareblis wlovaneba aris" + age)
+
+surname=input("please enter your surname: ")
+
+print("momxmareblis gvari aris" + surname)
