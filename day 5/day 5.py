fc79635b830b7f1dd0642cb9bf2c9c75451e01b5
chavle77867@gmail.com <chavle77867@gmail.com>
2024-02-29 20:43:50 +0400

    second commit

--- day 5/day 5.py
+++ day 5/day 5.py
@@ -13,3 +13,9 @@ print(num1 / num2)
 print(num1 > num2)
 
 print(num1 < num2)
+ 
+print(num1 >= num2)
+
+print(num1 <= num2)
+
+
---
+++ day 7/day 7.py
@@ -0,0 +1,16 @@
+for i in range(0,11,2)
+print(i)
+num=1
+
+
+i=1
+while i<20:
+    print(i)
+    i=i+1
+
+result=0
+i=0
+while i<=5:
+    result=result + i
+    i=i+1
+    print(result)
