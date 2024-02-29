7e14df58412c64d639f0defda6eaec4380b4d57f
chavle77867@gmail.com <chavle77867@gmail.com>
2024-02-29 21:34:37 +0400

    second commit

--- day 8/day 8.py
+++ day 8/day 8.py
@@ -1,12 +1,11 @@
 name="nika"
 password="nika123"
-
+name
 repeat_password=input()
 
-"if repeat_password != password"
-
-print("try again")
+if repeat_password != password:
+    print("try again")
 else:
+    print("authorized")
 
-print("authorized")
 
