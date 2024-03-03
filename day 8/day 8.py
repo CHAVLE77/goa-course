1515fb0f330b6715731f75785d8e996c5db915c5
chavle77867@gmail.com <chavle77867@gmail.com>
2024-03-03 23:18:26 +0400

    second commit

--- day 8/day 8.py
+++ day 8/day 8.py
@@ -1,7 +1,7 @@
-name="nika"
-password="nika123"
+name=input("enter your name:")
+password=input("enter your password:")
 
-repeat_password=input()
+repeat_password=input("enter your password again:bro it's just a brute force method vkbro it's just a brute force method kv")
 
 if repeat_password != password:
     print("try again")
